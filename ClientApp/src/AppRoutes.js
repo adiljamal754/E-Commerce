fb2950364f8c0868996c644545cef5f658182bbe
adiljamal754 <adilabudine@gmail.com>
2022-10-12 14:38:51 +0200

    Initial Commit

---
+++ ClientApp/src/AppRoutes.js
@@ -0,0 +1,15 @@
+import { Counter } from "./components/Counter";
+import { Home } from "./components/Home";
+
+const AppRoutes = [
+  {
+    index: true,
+    element: <Home />
+  },
+  {
+    path: '/counter',
+    element: <Counter />
+  }
+];
+
+export default AppRoutes;
